944c076915251b1fdfdb012c90d76dbad881a803
Nathan Allison <ncallison@wcu.edu>
2022-06-16 09:20:18 -0400

    updated

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+count = 1
+height = 100 * 0.6
+
+while count <= 10:
+    print (count, round(height,4))
+    height = height * 0.6
+    count = count + 1
---
+++ Work/name.py
@@ -0,0 +1,2 @@
+name = input('Enter your name: ')
+print('Haha you smell,', name)
